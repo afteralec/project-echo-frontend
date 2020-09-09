90cd91c4ca1d252b1fb58e7fca8ce89e92aad99f
Alec DuBois <after.alec@gmail.com>
2020-09-08 22:35:01 -0700

    Updates styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 initClickEvents();
+fetchUser(15).then(loadUser);
+fetchEchos(15).then(appendEchos);
 
 function initClickEvents() {
   const user = document.getElementById("user");
@@ -19,18 +21,43 @@ function toggleEchoForm() {
 }
 
 function appendListeners(listeners) {
-  const user = document.getElementById("user")
+  const user = document.getElementById("user");
+
   for (const listener of listeners) {
-    appendListener(user, listener)
+    appendListener(user, listener);
   }
 }
 
 function appendListener(element, listener) {
-  element.innerHTML += renderListener(listener)
+  element.innerHTML += renderListener(listener);
 }
 
 function renderListener(listener) {
-  return `<div class="bx-1 br-15"><h3>${listener.first_name} ${listener.last_name} - ${listener.status}</h3></div>`
+  return `<div class="bx-1 br-15"><h3>${listener.first_name} ${listener.last_name} - ${listener.status}</h3></div>`;
+}
+
+function appendEchos(echos) {
+  const feed = document.getElementById("feed");
+
+  for (const echo of echos) {
+    appendEcho(feed, echo);
+  }
+}
+
+function appendEcho(element, echo) {
+  element.innerHTML += renderEcho(echo);
+}
+
+function renderEcho(echo) {
+  return `
+  <div class="flow-left flex align-center pbr-bottom">
+    <div class="flex flex-col align-center flow-s">
+      <img class="round" src="https://www.gravatar.com/avatar/6a23f0f21cd556add3bd744ff812da04?d=robohash" />
+      <p class="f-down-1">${echo.user.first_name}</p>
+    </div>
+    <p>${echo.message}</p>
+  </div>
+  `;
 }
 
 function elementVisible(element) {
@@ -43,4 +70,20 @@ function hideElement(element) {
 
 function revealElement(element) {
   element.classList.remove("hidden");
+}
+
+function fetchUser(id) {
+  return fetch(`http://localhost:3000/api/v1/users/${id}`).then((resp) =>
+    resp.json()
+  );
+}
+
+function fetchEchos(id) {
+  return fetch(`http://localhost:3000/api/v1/echos/${id}`).then((resp) =>
+    resp.json()
+  );
+}
+
+function loadUser(user) {
+  appendListeners(user.listeners);
 }
